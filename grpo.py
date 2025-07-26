1e86f83c37a1cd20ad2528f2af3e49a34ebfdc17
Ilija <ilija.lichkovski@gmail.com>
2025-07-26 14:39:52 +0200

    int reward func is now a numeric reward func

--- grpo.py
+++ grpo.py
@@ -82,7 +82,7 @@ def correctness_reward_func(prompts, completions, answer, **kwargs) -> list[floa
     print('-'*20, f"Question:\n{q}", f"\nAnswer:\n{answer[0]}", f"\nResponse:\n{responses[0]}", f"\nExtracted:\n{extracted_responses[0]}")
     return [2.0 if r == a else 0.0 for r, a in zip(extracted_responses, answer)]
 
-def int_reward_func(completions, **kwargs) -> list[float]:
+def numeric_reward_func(completions, **kwargs) -> list[float]:
     responses = [completion[0]['content'] for completion in completions]
     extracted_responses = [extract_xml_answer(r) for r in responses]
     def is_numeric(s):
@@ -159,7 +159,7 @@ trainer = GRPOTrainer(
     reward_funcs=[
         correctness_reward_func,
         xmlcount_reward_func,
-        int_reward_func,
+        numeric_reward_func,
     ],
     args=training_args,
     train_dataset=dataset,
